8016187e6c3f81b236ee0aa0d27ea48c3e971d55
JacksStuff0905 <114748666+JacksStuff0905@users.noreply.github.com>
2024-04-14 10:56:40 +0200

    Added RuneInscriber menu

--- src/main/java/com/jacksstuff/magic_overhaul/screen/ModMenuTypes.java
+++ src/main/java/com/jacksstuff/magic_overhaul/screen/ModMenuTypes.java
@@ -1,6 +1,6 @@
-package com.jacksstuff.magicoverhaul.screen;
+package com.jacksstuff.magic_overhaul.screen;
 
-import com.jacksstuff.magicoverhaul.MagicOverhaul;
+import com.jacksstuff.magic_overhaul.MagicOverhaul;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.common.extensions.IForgeMenuType;
@@ -15,8 +15,8 @@ public class ModMenuTypes {
     public static final DeferredRegister<MenuType<?>> MENUS =
             DeferredRegister.create(ForgeRegistries.MENU_TYPES, MagicOverhaul.MOD_ID);
 
-    public static final RegistryObject<MenuType<RuneExtractorMenu>> RUNE_EXTRACTOR_MENU =
-            registerMenuType("rune_extractor_menu", RuneExtractorMenu::new);
+    public static final RegistryObject<MenuType<RuneInscriberMenu>> RUNE_INSCRIBER_MENU =
+            registerMenuType("rune_inscriber_menu", RuneInscriberMenu::new);
 
     public static final RegistryObject<MenuType<SpellCastMenu>> SPELL_CAST_MENU =
             registerMenuType("spell_cast_menu", SpellCastMenu::new);
